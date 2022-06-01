1f45ec36f3f1e5cbdc0e8d998df044d4fcc48d5d
ratan <ratanupadhyay6835@gmail.com>
2022-06-01 19:45:45 +0530

    passed tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-import KanbanBoard from './components/kanban-board/index.js';
-import 'h8k-components';
+import React, { Component } from "react";
+import "./App.css";
+import KanbanBoard from "./components/kanban-board/index.js";
+import "h8k-components";
 
 const title = "Kanban Board";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: this.props.tasks,
+    };
+    this.bwdClick=this.bwdClick.bind(this);
+    this.fwdClick=this.fwdClick.bind(this);
+  }
+
+   bwdClick(task) {
+    for (let i = 0; i < 10; ++i) {
+      if (this.state.tasks[i].name === task.name) {
+        let tasks = this.state.tasks;
+        tasks[i].stage = tasks[i].stage - 1;
+        this.setState({ tasks: tasks });
+      }
+    }
+  }
+ 
+
+  fwdClick(task){
+    for (let i = 0; i < 10; ++i) {
+      if (this.state.tasks[i].name === task.name) {
+        let oldTasks = this.state.tasks;
+        oldTasks[i].stage = oldTasks[i].stage + 1;
+        this.setState({ tasks: oldTasks });
+      }
+    }
+  };
+
+  
+
   render() {
     return (
       <div>
         <h8k-navbar header={title}></h8k-navbar>
-        <KanbanBoard tasks={this.props.tasks}/>
+        <KanbanBoard
+          tasks={this.state.tasks}
+          fwdClick={this.fwdClick}
+          bwdClick={this.bwdClick}
+        />
       </div>
     );
   }
--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 export default class KanbanBoard extends Component {
   constructor(props) {
     super(props);
-    // Each task is uniquely identified by its name. 
+    // Each task is uniquely identified by its name.
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
     this.state = {
-        tasks: this.props.tasks
+      tasks: this.props.tasks,
     };
-    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+    this.stagesNames = ["Backlog", "To Do", "Ongoing", "Done"];
   }
- 
+
   render() {
     const { tasks } = this.state;
 
@@ -27,34 +27,110 @@ export default class KanbanBoard extends Component {
     return (
       <div className="mt-20 layout-column justify-content-center align-items-center">
         <div className="mt-50 layout-row">
-            {stagesTasks.map((tasks, i) => {
-                return (
-                    <div className="card outlined ml-20 mt-0" key={`${i}`}>
-                        <div className="card-text">
-                            <h4>{this.stagesNames[i]}</h4>
-                            <ul className="styled mt-50" data-testid={`stage-${i}`}>
-                                {tasks.map((task, index) => {
-                                    return <li className="slide-up-fade-in" key={`${i}${index}`}>
-                                      <div className="li-content layout-row justify-content-between align-items-center">
-                                        <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
-                                        <div className="icons">
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
-                                            <i className="material-icons">arrow_back</i>
-                                          </button>
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
-                                            <i className="material-icons">arrow_forward</i>
-                                          </button>
-                                        </div>
-                                      </div>
-                                    </li>
-                                })}
-                            </ul>
-                        </div>
-                    </div>
-                )
-            })}
+          {stagesTasks.map((tasks, i) => {
+            return (
+              <div className="card outlined ml-20 mt-0" key={`${i}`}>
+                <div className="card-text">
+                  <h4>{this.stagesNames[i]}</h4>
+                  <ul className="styled mt-50" data-testid={`stage-${i}`}>
+                    {tasks.map((task, index) => {
+                      return (
+                        <li className="slide-up-fade-in" key={`${i}${index}`}>
+                          <div className="li-content layout-row justify-content-between align-items-center">
+                            <span
+                              data-testid={`${task.name
+                                .split(" ")
+                                .join("-")}-name`}
+                            >
+                              {task.name}
+                            </span>
+                            <div className="icons">
+                              {i === 0 ? (
+                                <>
+                                  <button
+                                    className="icon-only x-small mx-2"
+                                    disabled
+                                    data-testid={`${task.name
+                                      .split(" ")
+                                      .join("-")}-back`}
+                                  >
+                                    <i className="material-icons">arrow_back</i>
+                                  </button>
+                                  <button
+                                    className="icon-only x-small mx-2"
+                                    data-testid={`${task.name
+                                      .split(" ")
+                                      .join("-")}-forward`}
+                                      onClick={()=>this.props.fwdClick(task)}
+                                  >
+                                    <i className="material-icons">
+                                      arrow_forward
+                                    </i>
+                                  </button>
+                                </>
+                              ) : i === 3 ? (
+                                <>
+                                  <button
+                                    className="icon-only x-small mx-2"
+                                    onClick={()=>this.props.bwdClick(task)}
+                                    data-testid={`${task.name
+                                      .split(" ")
+                                      .join("-")}-back`}
+                                  >
+                                    <i className="material-icons">arrow_back</i>
+                                  </button>
+                                  <button
+                                    disabled
+                                    className="icon-only x-small mx-2"
+                                    data-testid={`${task.name
+                                      .split(" ")
+                                      .join("-")}-forward`}
+                                  >
+                                    <i className="material-icons">
+                                      arrow_forward
+                                    </i>
+                                  </button>
+                                </>
+                              ) : (
+                                <>
+                                  
+                                  <button
+                                    className="icon-only x-small mx-2"
+                                    onClick={()=>this.props.bwdClick(task)}
+                                    data-testid={`${task.name
+                                      .split(" ")
+                                      .join("-")}-back`}
+                                  >
+                                    <i className="material-icons">arrow_back</i>
+                                  </button>
+                                  <button
+                                    className="icon-only x-small mx-2"
+                                    onClick={()=>this.props.fwdClick(task)}
+                                    data-testid={`${task.name
+                                      .split(" ")
+                                      .join("-")}-forward`}
+                                      
+                                  >
+                                    <i className="material-icons">
+                                      arrow_forward
+                                    </i>
+                                  </button>
+                                </>
+                              )}
+                            </div>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
   }
 }
+
+
